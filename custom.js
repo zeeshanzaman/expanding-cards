ec741177c055b241c036b36f6ba8d5ae37fec213
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-09-08 12:08:52 +0500

    apply comments on line by line

--- custom.js
+++ custom.js
@@ -1,15 +1,20 @@
+// create a variable and apply querySelectorAll on it  
 const panels = document.querySelectorAll('.panel')
-
+    // apply for eachLoop
 panels.forEach((panel) => {
+    // create a function with add eventListner
     panel.addEventListener('click', () => {
+        // create a function with for removing classes
         removeActiveClasses()
+            // rest part of add eventListener to add classes
         panel.classList.add('active')
     })
 })
 
+// functio for remove classes
 function removeActiveClasses() {
     panels.forEach(panel => {
         panel.classList.remove('active')
-            // panel.classList.remove('active')
+
     })
 }
